9eeca41fa5c5cdd7f8968258e85c14ab3bc1f681
Juhaina Mufassir <110373605+juhaina1996@users.noreply.github.com>
2023-08-11 00:57:59 +0530

    Implemented react chat app using chat engine

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "5a2c04da-0417-4d23-a719-faed0d47c1f4";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      // onNewMessage={() =>
+      //   new Audio(
+      //     "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+      //   ).play()
+      // }
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
